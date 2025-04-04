3b5d2c37a9f1d52105a3bf56a314ac723e3618c5
DIO0550 <diot12345@yahoo.co.jp>
2025-04-04 14:51:26 +0000

    🎨 [Accessibility]: 石をひっくり返す時にcalculateDirectionalRotationを利用して方向に応じた回転アニメーションを適用

--- react-reversi/src/features/reversi/hooks/use-discs.tsx
+++ react-reversi/src/features/reversi/hooks/use-discs.tsx
@@ -4,6 +4,9 @@ import {
   BOARD_SIZE,
   DiscColor,
   Board,
+  CellState,
+  RotationState,
+  FlipDirection,
 } from '../types/reversi-types';
 import {
   findFlippableDiscs,
@@ -22,17 +25,29 @@ const ERROR_MESSAGES = {
  * 初期盤面の設定
  */
 const createInitialBoard = (): Board => {
-  const board = ArrayExtension.createMatrixWithLength<DiscColor>(
-    BOARD_SIZE,
-    DiscColor.NONE,
-  ) as Board;
+  const board = ArrayExtension.createMatrixWithLength<CellState>(BOARD_SIZE, {
+    discColor: DiscColor.NONE,
+    rotationState: RotationState.createInitial(),
+  }) as Board;
 
   const middle = BOARD_SIZE / 2 - 1;
 
-  board[middle][middle] = DiscColor.WHITE;
-  board[middle][middle + 1] = DiscColor.BLACK;
-  board[middle + 1][middle] = DiscColor.BLACK;
-  board[middle + 1][middle + 1] = DiscColor.WHITE;
+  board[middle][middle] = {
+    discColor: DiscColor.WHITE,
+    rotationState: RotationState.fromDiscColor(DiscColor.WHITE),
+  };
+  board[middle][middle + 1] = {
+    discColor: DiscColor.BLACK,
+    rotationState: RotationState.fromDiscColor(DiscColor.BLACK),
+  };
+  board[middle + 1][middle] = {
+    discColor: DiscColor.BLACK,
+    rotationState: RotationState.fromDiscColor(DiscColor.BLACK),
+  };
+  board[middle + 1][middle + 1] = {
+    discColor: DiscColor.WHITE,
+    rotationState: RotationState.fromDiscColor(DiscColor.WHITE),
+  };
 
   return board;
 };
@@ -42,6 +57,34 @@ const createInitialBoard = (): Board => {
  */
 const getOppositeColor = (color: DiscColor): DiscColor => {
   return color === DiscColor.BLACK ? DiscColor.WHITE : DiscColor.BLACK;
+};
+
+/**
+ * 位置関係から回転方向を決定する関数
+ */
+const determineFlipDirection = (
+  sourcePos: BoardPosition,
+  targetPos: BoardPosition,
+): FlipDirection => {
+  // 行の差分
+  const rowDiff = targetPos.row - sourcePos.row;
+  // 列の差分
+  const colDiff = targetPos.col - sourcePos.col;
+
+  // 水平方向（左右）の動きが主要な場合
+  if (Math.abs(colDiff) > Math.abs(rowDiff)) {
+    // 右向きか左向き
+    return colDiff > 0
+      ? FlipDirection.LEFT_TO_RIGHT
+      : FlipDirection.RIGHT_TO_LEFT;
+  }
+  // 垂直方向（上下）の動きが主要または同等の場合
+  else {
+    // 下向きか上向き
+    return rowDiff > 0
+      ? FlipDirection.TOP_TO_BOTTOM
+      : FlipDirection.BOTTOM_TO_TOP;
+  }
 };
 
 /**
@@ -94,11 +137,31 @@ export const useDiscs = () => {
     const newBoard = board.map((row) => [...row]);
 
     // 置いた位置に石を追加
-    newBoard[position.row][position.col] = currentTurn;
+    newBoard[position.row][position.col] = {
+      discColor: currentTurn,
+      rotationState: RotationState.fromDiscColor(currentTurn),
+    };
 
     // ひっくり返す石を処理
     flippableDiscs.forEach((flipPosition) => {
-      newBoard[flipPosition.row][flipPosition.col] = currentTurn;
+      // 現在の石の回転状態を取得
+      const currentRotationState =
+        board[flipPosition.row][flipPosition.col].rotationState;
+
+      // 回転方向を決定（置いた石の位置からひっくり返す石への方向）
+      const flipDirection = determineFlipDirection(position, flipPosition);
+
+      // 回転方向に基づいて新しい回転状態を計算
+      const newRotationState = RotationState.calculateDirectionalRotation(
+        currentRotationState,
+        flipDirection,
+      );
+
+      // 石をひっくり返す
+      newBoard[flipPosition.row][flipPosition.col] = {
+        discColor: currentTurn,
+        rotationState: newRotationState,
+      };
     });
 
     // 盤面と手番を更新
