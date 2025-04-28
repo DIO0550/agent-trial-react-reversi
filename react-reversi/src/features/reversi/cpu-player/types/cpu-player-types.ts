54cba98c4daa761e263b708811fb3dbf553bb07b
DIO0550 <diot12345@yahoo.co.jp>
2025-04-28 13:45:22 +0000

    ✨ [New Feature]: CpuPlayerLevelに最強(strongest)レベルを追加し、Point型の参照をBoardPosition型に修正

--- react-reversi/src/features/reversi/cpu-player/types/cpu-player-types.ts
+++ react-reversi/src/features/reversi/cpu-player/types/cpu-player-types.ts
@@ -1,13 +1,13 @@
-import { Board, Point } from '../../types/reversi-types';
+import { Board, BoardPosition } from '@/features/reversi/types/reversi-types';
 
 /**
  * CPUプレイヤーのインターフェース
  */
 export type CpuPlayer = {
-  calculateNextMove: (board: Board, currentPlayer: number) => Point;
+  calculateNextMove: (board: Board, currentPlayer: number) => BoardPosition;
 };
 
 /**
  * CPUプレイヤーの難易度レベル
  */
-export type CpuPlayerLevel = 'weak' | 'normal' | 'strong';
+export type CpuPlayerLevel = 'weak' | 'normal' | 'strong' | 'strongest';
